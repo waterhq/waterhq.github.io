012a1060b01ef6c5ce8f6068dae105ebc0c55c1e
waterhq <134560970+waterhq@users.noreply.github.com>
2023-05-24 19:48:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+let score = 0;
+
+function addPoint() {
+  score++;
+  updateScore();
+  createHeart();
+}
+
+function updateScore() {
+  const scoreElement = document.getElementById('score');
+  scoreElement.innerText = score;
+}
+
+function createHeart() {
+  const heartsContainer = document.getElementById('hearts');
+  const heart = document.createElement('span');
+  heart.className = 'heart';
+  heart.innerText = '❤';
+  heartsContainer.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 2000);
+}
 
